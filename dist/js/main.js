1a34240909b0d0de0a96ae322e4cdc042fb58f8d
stdunn <seantd@live.com>
2016-08-26 10:02:57 -0700

    Latest version

---
+++ dist/js/main.js
@@ -0,0 +1,28 @@
+$('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        constrain_width: false,
+        hover: true,
+        gutter: 0,
+        belowOrigin: true,
+        alignment: 'right'
+    }
+);
+$(document).ready(function(){
+    $('.carousel').carousel({
+        indicators: true
+    });
+});
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+});
+$(document).ready(function(){
+    $('.slider').slider({
+    });
+});
+$('.carousel.carousel-slider').carousel({full_width: true});
+$(document).ready(function(){
+    $('.parallax').parallax();
+});
+
+
